9bd52fae7dfcaff0fb19955083de3ef74d3ca41f
JQJ-PC\JQJ <245002672@qq.com>
2021-07-17 18:02:32 +0800

    commit end data

--- service/service_hosp/src/main/java/com/qijie/yygh/mapper/HospitalSetMapper.java
+++
@@ -1,6 +0,0 @@
-package com.qijie.yygh.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface HospitalSetMapper extends BaseMapper<model.hosp.HospitalSet> {
-}
--- service/service_hosp/src/main/java/com/qijie/yygh/service/HospitalSetService.java
+++
@@ -1,6 +0,0 @@
-package com.qijie.yygh.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface HospitalSetService extends IService<model.hosp.HospitalSet> {
-}
